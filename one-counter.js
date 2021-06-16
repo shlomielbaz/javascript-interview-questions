3ab1571ad0e12ecb3f30b2968ebf6623bf6d6207
Shlomi Elbaz <shlomisderot@gmail.com>
2021-06-16 21:07:45 +0300

    count number of 1s in the binary representation
    Efficient way to count number of 1s in the binary representation of a number in O(1)

--- one-counter.js
+++ one-counter.js
@@ -4,9 +4,8 @@
 function bitCount(u)
 {
     var uCount;
-
-     uCount = u - ((u >> 1) & 033333333333) - ((u >> 2) & 011111111111);
-     return ((uCount + (uCount >> 3)) & 030707070707) % 63;
+    uCount = u - ((u >> 1) & 033333333333) - ((u >> 2) & 011111111111);
+    return ((uCount + (uCount >> 3)) & 030707070707) % 63;
 }
 
 function bitCount(x){
