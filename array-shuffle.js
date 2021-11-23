3aa6aeeba3a48d383c5a40c8480691f3b807a957
Shlomi Elbaz <shlomisderot@gmail.com>
2021-11-23 20:44:56 +0200

    Update array-shuffle.js

--- array-shuffle.js
+++ array-shuffle.js
@@ -4,7 +4,7 @@
 
 const arr = [1, 2, 3, 4, 5, 6, 8]
 
-
+// using extra array
 function shuffle (arr) {
     const stack = [];
   
@@ -15,4 +15,17 @@ function shuffle (arr) {
     while (arr.length > 0)
       
     return stack;
+}
+
+// without using extra array
+function shuffle (arr) {
+    let len = arr.length;
+  
+    do {
+        let index = parseInt(0 + Math.random() * (len - 0))
+        arr.push(arr.splice(index, 1)[0]);
+    }
+    while ((--len) > 0)
+    
+    return arr
 }
