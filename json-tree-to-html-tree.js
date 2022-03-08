cd8164bb051ae4a0e68ce05e3afb3c7b162f2b7f
Shlomi Elbaz <shlomisderot@gmail.com>
2022-03-08 18:43:41 +0200

    Update json-tree-to-html-tree.js

--- json-tree-to-html-tree.js
+++ json-tree-to-html-tree.js
@@ -48,85 +48,121 @@ will generate the HTML:
 
 Online demo: https://playcode.io/870129/
 */
-var stack = [];
-
-var ul = null;
-var level = 1
-var data = {
-    "tag": "ul",
-    "children": [
-        {
-            "tag": "li",
-            "text": "item 1"
-        },
-        {
-            "tag": "li",
-            "text": "item 2"
-        },
-        {
-            "tag": "li",
-            "text": "item 3",
-            "children": [
-                {
-                    "tag": "ul",
-                    "children": [
-                        {
-                            "tag": "li",
-                            "text": "item 3.1"
-                        },
-                        {
-                            "tag": "li",
-                            "text": "item 3.2"
-                        }
-                    ]
-                }
-            ]
-        }
-    ]
+const data = {
+	tag: 'ol',
+	children: [
+		{
+			tag: 'li',
+			text: 'item 1',
+		},
+		{
+			tag: 'li',
+			text: 'item 2',
+		},
+		{
+			tag: 'li',
+			text: 'item 3',
+			children: [
+				{
+					tag: 'ul',
+					children: [
+						{
+							tag: 'li',
+							text: 'item 3.1',
+						},
+						{
+							tag: 'li',
+							text: 'item 3.2',
+						},
+						{
+							tag: 'li',
+							text: 'item 3.3',
+						},
+						{
+							tag: 'li',
+							text: 'item 3.4',
+							children: [
+								{
+									tag: 'ul',
+									children: [
+										{
+											tag: 'li',
+											text: 'item 3.4.1',
+										},
+										{
+											tag: 'li',
+											text: 'item 3.4.2',
+										},
+										{
+											tag: 'li',
+											text: 'item 3.4.3',
+										},
+										{
+											tag: 'li',
+											text: 'item 3.4.4',
+										}
+									]
+								}
+							]
+						}
+					]
+				}
+			]
+		}
+	]
 };
+
+let result = '';
+const stack = [];
 
 const spaces = (indentation, level) => {
-    var space = ''
-    var len = indentation * level
+	var space = '';
+	var len = indentation * level;
 
-    for (var j = 0; j < len; j++) {
-        space += ' '
-    }
+	for (var j = 0; j < len; j++) {
+		space += ' ';
+	}
 
-    return space
-}
-
-
-const jsonTreeToHTMLList = (data, indentation, level = 0, result = '') => {
-    let ul = data['tag'];
-    let space = spaces(indentation, level);
-    result += `${space}<${ul}>\n`;
-    stack.unshift(`${space}</${ul}>\n`);
-    const children = data['children'];
-    level = level + 1;
-	
-    for (let i = 0; i < children.length; i++) {
-        let item = children[i];
-
-        let li = item['tag'];
-        let text = item['text'];
-        space = spaces(indentation, level);
-
-
-        if ('children' in item) {
-            result += `${space}<${li}>${text}\n`;
-            stack.unshift(`${space}</${li}>\n`);
-
-            return jsonTreeToHTMLList(item['children'][0], indentation, (level + 1), result);
-        }
-        else {
-          result += `${space}<${li}>${text}</${li}>\n`;
-        }
-    }
-    
-    result += stack.join("");
-    
-    console.log(result);
+	return space;
 };
 
-jsonTreeToHTMLList(data, 4)
+const jsonTreeToHTMLList = (data, indentation, level = 0) => {
+	let ul = data['tag'];
+
+	let space = spaces(indentation, level);
+
+	result += `${space}<${ul}>\n`;
+
+	stack.unshift(`${space}</${ul}>\n`);
+
+	const children = data['children'];
+	level = level + 1;
+
+	for (let i = 0; i < children.length; i++) {
+		let item = children[i];
+
+		let li = item['tag'];
+		let text = item['text'];
+		space = spaces(indentation, level);
+
+		if ('children' in item) {
+			result += `${space}<${li}>${text}\n`;
+			stack.unshift(`${space}</${li}>\n`);
+
+			return jsonTreeToHTMLList(
+				item['children'][0],
+				indentation,
+				(level + 1),
+				result
+			);
+		} else {
+			result += `${space}<${li}>${text}</${li}>\n`;
+		}
+	}
+
+	result += stack.join('');
+
+	console.log(result);
+};
+
+jsonTreeToHTMLList(data, 4);
