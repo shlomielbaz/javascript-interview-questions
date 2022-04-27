19265c1eea4e997a739903d86faf9fbe764e7f8e
Shlomi Elbaz <shlomisderot@gmail.com>
2022-04-27 06:53:37 +0300

    Update sleep.js

--- sleep.js
+++ sleep.js
@@ -1,11 +1,11 @@
 // implement C# Thread.Sleep like function in javascript
 
 const sleep = (seconds) => {
-  const startTime = new Date();
+  const now = new Date();
   
   const miliseconds = seconds * 1000;
   
-  while (new Date() - startTime < miliseconds) { }
+  while (new Date() - now < miliseconds) { }
   
   return true;
 }
