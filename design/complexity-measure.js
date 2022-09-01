6bc1b85d631761fb87fd8879b5eb23eb5fce29ab
Shlomi Elbaz <shlomisderot@gmail.com>
2022-09-01 19:40:49 +0300

    Update complexity-measure.js

--- design/complexity-measure.js
+++ design/complexity-measure.js
@@ -1,19 +1,48 @@
 // assume n is known and you have an array of n length while the array items are 0 to n-1.
 // our array is not sorted.
 n=5;
-arr=[3,4,0,2,1];
+arr = [3,4,0,2,1];
 
-// imaging a mouse came and eat one of the array items, after the mouse eating our array looks like the following:
-arr=[3,4,0,0,1]
+// imaging a mouse came and eat one of the array items, when mouse eats item is set to zero
+//  after the mouse eat one item our array looks like the following:
+arr = [3,4,0,0,1] --> (2)
 
 // our task is to know what item was eatten?
 
-// Solution #1
-// Define a dictionary when the dictionary keys are from zero to n-1, and then the missing key is the eatten item.
+// solution #1 - Dictionary + Scan
+// 1. loop over the array items and set each item as a dictionary key.
+// 2. loop over n and check each index is exist in the dictionary keys.
+// complexity:
 // - Runtime: O(n)
 // - Space: O(n)
 
-// Solution #2
+// solution #2 - Sort + Scan
 // Sort the array and then check if the next item is greater in 1 from the current item, if not, the current item + 1 item is eatten
+// complexity:
+// - Runtime: O(nlogn)
+// - Space: O(1)
 
-// Solution #3
+// solution #3 - Math Operation
+// sum(orginal) - sum(current) = eatten item 
+// complexity:
+// - Runtime: O(n)
+// - Space: O(1)
+
+// now, what if the mouse ate two items?
+// after the mouse eat two items our array looks like the following:
+arr = [0,4,0,0,1] --> (3, 2)
+
+// in this case we don't need to modufy nothing in Solution #1 and Solution #2
+
+// solution #3 - Math Operation
+// mul(orginal) / mul(current) = X, Y
+// complexity:
+// - Runtime: O(n)
+// - Space: O(1)
+
+// a prove:
+1 * 2 * 3 * 4
+-------------- = 2 * 3
+1 *         4
+
+
