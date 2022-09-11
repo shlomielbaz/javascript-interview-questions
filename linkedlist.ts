d32e3228e622c900779bef1b04145a2fb474cb3a
Shlomi Elbaz <shlomisderot@gmail.com>
2022-09-11 08:08:32 +0300

    Add files via upload

---
+++ linkedlist.ts
@@ -0,0 +1,60 @@
+
+
+
+class LinkedListNode {
+    value: number;
+    next: LinkedListNode | null;
+
+    constructor(value: number) {
+        this.value = value;
+        this.next = null
+    }
+}
+
+
+class LinkedList {
+    private head: LinkedListNode | null = null;
+
+    add(value: number, is_unique: boolean = false) {
+        const node = new LinkedListNode(value);
+        if (this.head === null || this.head === undefined) {
+            this.head = node;
+        }
+        else if (this.head.value > node.value) {
+            node.next = this.head;
+            this.head = node
+        }
+        else {
+            let current = this.head;
+            while (current != null) {
+                if (current.next === null) {
+                    current.next = node;
+                    break;
+                }
+                else if (is_unique && (current.value === node.value)) {
+                    break;
+                }
+                else if ((current.value === node.value) || (current.value < node.value && current.next.value > node.value)) {
+                    node.next = current.next;
+                    current.next = node;
+                    break
+                }
+                current = current.next;
+            }
+        }
+        this.print();
+    }
+
+    print() {
+        let current = this.head;
+        const view = [];
+
+        while (current != null) {
+            view.push(`[${current.value}]`);
+            current = current.next;
+        }
+
+        view.push('NULL');
+        console.log(view.join(' -> '));
+    }
+}
