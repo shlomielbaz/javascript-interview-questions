a3c770ba19fb52b4cb374af3a1b6b3271d115787
Shlomi Elbaz <shlomi.elbaz@outlook.com>
2024-01-08 10:32:01 +0200

    Create tests.js

---
+++ tests.js
@@ -0,0 +1,39 @@
+// const arr = [1,2,3,4,5,6,7,8,9,10,11];
+
+// const n = 10;
+
+// let len = arr.length;
+
+// while (len > 0) {
+//     if (arr[len] > n) {
+//         arr.pop();
+//     }
+//     else {
+//         break;
+//     }
+//     len = len - 1;
+// }
+// arr = [1,2,3,5,6,7,8,9,10]; i = 0
+// arr = [1,2,3,5,6,7,8,9]; i = 1 
+// arr = [1,2,3,5,6,7,8]; i = 2
+// arr = [1,2,3,5,6,7]; i = 3
+// arr = [1,2,3,5,6]; i = 4
+
+// len = arr.length;
+// for(const i = arr.length; i>0; i++) {
+//     for (const j = 0; j < i; j++) {
+//         (arr[i] + arr[j])
+//     }
+// }
+
+const arr = [2,4,5,6,9,10,11];
+const n = 10;
+
+const pairs = [];
+
+let i = 0;
+let j = 6;
+
+while (i<j) {
+    
+}
