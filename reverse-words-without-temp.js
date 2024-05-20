7fb32beea030dd833705c1cb12489f7b8f26e71d
Shlomi Elbaz <shlomi.elbaz@outlook.com>
2024-05-20 10:03:04 +0300

    Create reverse-words-without-temp.js

---
+++ reverse-words-without-temp.js
@@ -0,0 +1,35 @@
+/// reverse words without using any extra temporary variable,
+//  'Hello Guys, How are you' => you are How Guys, Hello
+
+
+const reverseWords = function (s) {
+   const words = s.split('');
+
+   reverse(words, 0 , words.length);
+
+   let start = 0;
+   let i = 0;
+
+   for (const word of words) {
+      if (word == ' ') {
+         reverse(words, start, i);
+         start = i+1;
+      }
+
+      i = i + 1;
+   }
+ 
+   reverse(words, start, i);
+
+   return words.join('');
+ }
+ 
+ function reverse(arr, start, end) {
+   const pivot = ((start+end)/2)
+    for (let i = start; i<pivot; i++) {
+       [arr[i],arr[end-1-(i-start)]] =  [arr[end-1-(i-start)], arr[i]];
+    }
+ }
+
+
+ console.log(reverseWords('Hello Guys, How are you'))
